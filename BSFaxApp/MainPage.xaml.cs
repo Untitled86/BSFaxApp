2fe9c2562caea55975a8bec5001d1c53a0f78b8e
Brien Schultz <bschultz@suite304.com>
2011-11-06 19:18:06 -0800

    First commit

---
+++ BSFaxApp/MainPage.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Collections;
+using Microsoft.Phone.Tasks;
+
+namespace BSFaxApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ApplicationBar.Buttons.Clear();
+            setupButtonIcons();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);            
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //ApplicationBar.IsVisible = false;
+            /*if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }*/
+        }
+
+        private void ListBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //((BSFaxApp.ItemViewModel)e.AddedItems[0]).LineOne
+            Console.Write(sender.ToString());
+            Microsoft.Phone.Tasks.PhotoChooserTask task = new Microsoft.Phone.Tasks.PhotoChooserTask();
+            task.Completed += new EventHandler<Microsoft.Phone.Tasks.PhotoResult>(task_Completed);
+            task.Show();
+        }
+
+        void task_Completed(object sender, Microsoft.Phone.Tasks.PhotoResult e)
+        {
+            if (e.TaskResult == Microsoft.Phone.Tasks.TaskResult.OK)
+            {
+                MessageBox.Show("" + e.ChosenPhoto.ToString());                
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.Write(sender.ToString());
+            
+            Microsoft.Phone.Tasks.PhoneNumberChooserTask task = new Microsoft.Phone.Tasks.PhoneNumberChooserTask();
+            
+            task.Completed += new EventHandler<Microsoft.Phone.Tasks.PhoneNumberResult>(task_Completed);
+            task.Show();
+        }
+
+        void task_Completed(object sender, Microsoft.Phone.Tasks.PhoneNumberResult e)
+        {
+            if (e.TaskResult == Microsoft.Phone.Tasks.TaskResult.OK)
+            {
+                MessageBox.Show(e.PhoneNumber);
+            }
+        }
+
+        ApplicationBarIconButton syncIcon = null;
+        ApplicationBarIconButton addIcon = null;
+        ApplicationBarIconButton uploadIcon = null;        
+
+        private void setupButtonIcons()
+        {
+            syncIcon = getIconFromName("sync", "/icons/appbar.sync.rest.png");
+            addIcon = getIconFromName("add pages", "/icons/appbar.add.rest.png");
+            uploadIcon = getIconFromName("send", "/icons/appbar.upload.rest.png");
+        }
+
+        private void Panorama_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {/*
+            string header = ((PanoramaItem)((Panorama)sender).SelectedItem).Header.ToString();
+            
+            ApplicationBar.Buttons.Clear();
+
+            if (header.Equals("quick fax", StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (ApplicationBar.Buttons.IndexOf(addIcon) == -1)
+                {
+                    ApplicationBar.Buttons.Add(addIcon);
+                }
+                ApplicationBarIconButton sendButton = uploadIcon;
+                sendButton.Click += new EventHandler(sendButton_Click);
+
+                if (ApplicationBar.Buttons.IndexOf(sendButton) == -1)
+                {
+                    ApplicationBar.Buttons.Add(sendButton);                
+                }
+
+                ApplicationBar.IsVisible = true;
+            }
+            else if ((header.Equals("how it works", StringComparison.InvariantCultureIgnoreCase)) ||
+                     (header.Equals("templates", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                ApplicationBar.IsVisible = true;
+            }
+            else if (header.Equals("sent faxes", StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (ApplicationBar.Buttons.IndexOf(syncIcon) == -1)
+                {
+                    ApplicationBar.Buttons.Add(syncIcon);
+                }
+                ApplicationBar.IsVisible = true;
+            }
+            else if (header.Equals("received faxes", StringComparison.InvariantCultureIgnoreCase))
+            {
+
+                if (ApplicationBar.Buttons.IndexOf(syncIcon) == -1)
+                {
+                    ApplicationBar.Buttons.Add(syncIcon);
+                }
+                ApplicationBar.IsVisible = true;
+            }
+            else
+            {
+                ApplicationBar.IsVisible = true;
+            }*/
+
+        }
+
+        void sendButton_Click(object sender, EventArgs e)
+        {
+            //System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            //System.IO.MemoryStream memoryStream = new MemoryStream(encoding.GetBytes("this is a test"));
+            //UploadUtil.StartUpload("http://brian_18_or@suite304.com:byeG00d@www.suite304.com/test.txt", memoryStream);
+            //UploadUtil.Uploads.Add(
+            FaxService.Version1Client wsClient = new FaxService.Version1Client();
+            wsClient.PingCompleted += new EventHandler<FaxService.PingCompletedEventArgs>(wsClient_PingCompleted);
+            wsClient.PingAsync();
+
+            FaxService.Fax fax = new FaxService.Fax();
+
+            System.Collections.ObjectModel.ObservableCollection<string> recipients = new System.Collections.ObjectModel.ObservableCollection<string>();
+            recipients.Add("Brien Schultz");
+            fax.Recipients = recipients;
+            wsClient.SendFaxCompleted += new EventHandler<FaxService.SendFaxCompletedEventArgs>(wsClient_SendCompleted);            
+            wsClient.SendFaxAsync(fax);
+            MessageBox.Show("Fax started...");
+
+            
+            WriteableBitmap wb = new WriteableBitmap(300, 300);
+            byte[] data = WriteableBitmapExtensions.ToByteArray(wb);
+            
+            
+        }
+
+        void wsClient_SendCompleted(object sender, FaxService.SendFaxCompletedEventArgs e)
+        {
+            MessageBox.Show("Fax completed.");
+        }
+
+        void wsClient_PingCompleted(object sender, FaxService.PingCompletedEventArgs e)
+        {
+            MessageBox.Show("Ping Result: " + e.Result.ToString());
+        }
+
+        private ApplicationBarIconButton getIconFromName(string aTitle, string aIconUri)
+        {
+            ApplicationBarIconButton result = new ApplicationBarIconButton();
+            result.Text = aTitle;
+            result.IsEnabled = true;
+            result.IconUri = new Uri(aIconUri, UriKind.Relative);
+            return result;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            WebBrowserTask task = new WebBrowserTask();
+            task.URL = Uri.EscapeUriString("https://www.metrofax.com/soho/signupform.aspx?referringparty=Brien Schultz&referral=Yes");
+            task.Show();
+        }
+    }
+}
---
+++ BSFaxApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+
+namespace BSFaxApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Templates = new ObservableCollection<ItemViewModel>();
+            this.CurrentFax = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Templates { get; private set; }
+
+        public ObservableCollection<ItemViewModel> CurrentFax { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData()
+        {
+            // Sample data; replace with real data
+            this.Templates.Add(new ItemViewModel() { LineOne = "Order coffee", LineTwo = "Used to order coffee for pickup or delivery.", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Templates.Add(new ItemViewModel() { LineOne = "Order food", LineTwo = "Used to order food for pickup or delivery.", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+            this.Templates.Add(new ItemViewModel() { LineOne = "Order something else", LineTwo = "Used to order something.", LineThree = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+
+            this.CurrentFax.Add(new ItemViewModel() { LineOne = "Coversheet", LineTwo = "Click this to change or delete the page.", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.CurrentFax.Add(new ItemViewModel() { LineOne = "Page 1", LineTwo = "Click this to change or delete the page.", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
